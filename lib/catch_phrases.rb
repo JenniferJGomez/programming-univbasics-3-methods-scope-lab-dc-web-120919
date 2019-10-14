294f4bbe0acfcaf25b0997a6d303b26287259147
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-10-14 15:33:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,7 +13,8 @@ def link
   puts phrase
 end
 
-def any_phrase(phrase)
-    phrase = "Do A Barrel Roll!\n".to_stdout
-    puts phrase
-end
+describe "any_phrase" do
+  it "takes in an argument and puts out the catch phrase" do
+    phrase = "Do A Barrel Roll!"
+    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
+  end
